188a7db37c505c565bcfcd6d1f0d7574e10c723a
jodyvole <jodyvole@student.21-school.ru>
2025-08-06 21:01:14 +0300

    created wallet mapper

---
+++ WalletService/src/main/java/org/jodyvole/walletservice/datasource/mappers/WalletMapper.java
@@ -0,0 +1,19 @@
+package org.jodyvole.walletservice.datasource.mappers;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.jodyvole.walletservice.datasource.entities.WalletEntity;
+import org.jodyvole.walletservice.domain.model.Wallet;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class WalletMapper {
+
+  public static Wallet toDomain(WalletEntity walletEntity) {
+    return new Wallet(walletEntity.getId(), walletEntity.getBalance());
+  }
+
+  public static WalletEntity toDatasource(Wallet wallet) {
+    return new WalletEntity(wallet.getWalletId(), wallet.getBalance());
+  }
+
+}
