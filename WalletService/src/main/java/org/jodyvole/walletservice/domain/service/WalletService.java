07295b7d72f81d757227ff66f770f02938b38200
jodyvole <jodyvole@student.21-school.ru>
2025-08-07 15:09:39 +0300

    created walletService interface

---
+++ WalletService/src/main/java/org/jodyvole/walletservice/domain/service/WalletService.java
@@ -0,0 +1,21 @@
+package org.jodyvole.walletservice.domain.service;
+
+import org.jodyvole.walletservice.domain.model.OperationType;
+import org.jodyvole.walletservice.domain.model.Transaction;
+import org.jodyvole.walletservice.domain.model.Wallet;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+public interface WalletService {
+
+  Wallet create();
+
+  Transaction processOperation(UUID walletId, OperationType operationType, Long amount);
+
+  BigDecimal getBalance(UUID walletId);
+
+  List<Transaction> getAllOperations(UUID walletId);
+
+}
