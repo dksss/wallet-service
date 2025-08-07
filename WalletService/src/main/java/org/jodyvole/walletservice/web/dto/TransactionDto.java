462b1c087cf1ca414e4857b2f04d04116579d91b
jodyvole <jodyvole@student.21-school.ru>
2025-08-07 15:57:17 +0300

    created dto's

---
+++ WalletService/src/main/java/org/jodyvole/walletservice/web/dto/TransactionDto.java
@@ -0,0 +1,24 @@
+package org.jodyvole.walletservice.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionDto {
+
+  private UUID id;
+  private UUID walletId;
+  private String operation;
+  private BigDecimal amount;
+  private LocalDateTime createdAt;
+
+}
---
+++ WalletService/src/main/java/org/jodyvole/walletservice/web/dto/WalletDto.java
@@ -0,0 +1,20 @@
+package org.jodyvole.walletservice.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WalletDto {
+
+  private UUID id;
+  private BigDecimal balance;
+
+}
