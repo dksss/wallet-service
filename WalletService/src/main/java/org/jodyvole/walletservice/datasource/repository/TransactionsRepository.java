9c22d23effa6f9d9790a2d4177616ef854a2303f
jodyvole <jodyvole@student.21-school.ru>
2025-08-07 15:09:19 +0300

    added method to repo

--- WalletService/src/main/java/org/jodyvole/walletservice/datasource/repository/TransactionsRepository.java
+++ WalletService/src/main/java/org/jodyvole/walletservice/datasource/repository/TransactionsRepository.java
@@ -1,11 +1,21 @@
 package org.jodyvole.walletservice.datasource.repository;
 
 import org.jodyvole.walletservice.datasource.entities.TransactionEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TransactionsRepository extends CrudRepository<TransactionEntity, UUID> {
+
+  @Query("SELECT t " +
+          "FROM TransactionEntity t " +
+          "WHERE t.wallet.id = :walletId " +
+          "ORDER BY t.createdAt DESC")
+  List<TransactionEntity> findAllOperationsByWallet(@Param("walletId") UUID walletId);
+
 }
